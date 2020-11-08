a2be7fd0f9808a0c6d07d28a316972e13e453e29
steven cao <stevencao23@gmail.com>
2020-11-08 03:12:43 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,9 @@
-# Write your code here
+count = 10
+new_year = 0 
+
+while count <= 0 do
+  puts count
+  count = count - 1
+  break if count = new_year do
+    puts "Happy New Year!"
+end
---
+++ testing.rb
@@ -0,0 +1,8 @@
+count = 10
+new_year = 0 
+
+while count = 0 do
+  puts count
+  count = count - 1
+  
+end
